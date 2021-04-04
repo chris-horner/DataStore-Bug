2f168942a548cc1016d6c94cbbfaf667f07fb4e2
Chris Horner <chris@horner.chat>
2021-04-04 11:07:43 +1000

    Demonstrate leak in alpha08

--- README.md
+++ README.md
@@ -1,19 +1,9 @@
 # DataStore Bug
 
-This is a tiny little project to demonstrate what might be an issue with Google's [DataStore](https://developer.android.com/topic/libraries/architecture/datastore) library.
+This is a tiny little project to demonstrate issues found in [DataStore](https://developer.android.com/topic/libraries/architecture/datastore).
 
-It attempts to make use of [Okio](https://square.github.io/okio)'s `CipherSource` and `CipherSink` to encrypt and decrypt the data saved inside of a `DataStore`.
+## Current Demo
 
-However something goes wrong when attempting to write the data to disk. `CipherSink` needs to close the `OutputStream` that's provided in `Serializer`'s `writeTo()` method, however this causes DataStore to crash, throwing the following exception:
-```
-java.io.SyncFailedException: sync failed
-    at java.io.FileDescriptor.sync(Native Method)
-    at androidx.datastore.SingleProcessDataStore.writeData$datastore_core_release(SingleProcessDataStore.kt:299)
-    at androidx.datastore.SingleProcessDataStore.transformAndWrite(SingleProcessDataStore.kt:282)
-    at androidx.datastore.SingleProcessDataStore$actor$1.invokeSuspend(SingleProcessDataStore.kt:165)
-```
+`PreferenceDataStoreSingletonDelegate` and `DataStoreSingletonDelegate` leak `Context` in their `produceFile` lambdas.
 
-Which is the result of `SingleProcessDataStore` attempting to call:
-```kotlin
-stream.fd.sync()
-```
+To demonstrate, run the sample app and either rotate the screen or press the back button. LeakCanary will report the leak.
--- app/src/main/java/codes/chrishorner/datastorebug/MainActivity.kt
+++ app/src/main/java/codes/chrishorner/datastorebug/MainActivity.kt
@@ -1,56 +1,16 @@
 package codes.chrishorner.datastorebug
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import androidx.datastore.Serializer
-import androidx.datastore.createDataStore
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
-import okio.buffer
-import java.io.InputStream
-import java.io.OutputStream
+import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore by preferencesDataStore("settings")
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-  private val scope = MainScope()
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    scope.launch {
-      val firstStore = createDataStore("encrypted_string", EncryptedStringSerializer)
-      val initialValue = firstStore.data.first()
-      Log.d("DataStore", "initialValue = $initialValue")
-      firstStore.updateData { "hello" }
-
-      val secondStore = createDataStore("encrypted_string", EncryptedStringSerializer)
-      val secondValue = secondStore.data.first()
-      Log.d("DataStore", "secondValue = $secondValue")
-    }
-  }
-
-  override fun onDestroy() {
-    super.onDestroy()
-    scope.cancel()
-  }
-}
-
-object EncryptedStringSerializer : Serializer<String> {
-
-  override fun readFrom(input: InputStream): String {
-    if (input.available() == 0) return ""
-
-    return IoEncryption.getDecryptedSource(input).buffer().use {
-      it.readString(Charsets.UTF_8)
-    }
-  }
-
-  override fun writeTo(t: String, output: OutputStream) {
-    // 🚨 It's this call that fails! 🚨
-    IoEncryption.getEncryptedSink(output).buffer().use {
-      it.writeString(t, Charsets.UTF_8)
-    }
+    val store = dataStore
   }
 }
